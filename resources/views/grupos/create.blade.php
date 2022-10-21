915310d215706af90dd25dfebe56c761e55b5ea0
MOMERO13 <18690293@tecvalles.mx>
2022-10-21 11:09:22 -0500

    añade campo instructor

--- resources/views/grupos/create.blade.php
+++ resources/views/grupos/create.blade.php
@@ -98,6 +98,21 @@
         </div>
     </div>
 
+    <div class="row mb-3">
+        <label for="instructor_id" class="col-md-4 col-form-label text-md-end">Instructor</label>
+
+        <div class="col-md-6">
+            <input id="instructor_id" type="text" class="form-control @error('instructor_id') is-invalid @enderror" name="instructor_id" value="{{ old('instructor_id') }}" required >
+
+            @error('instructor_id')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+    </div>
+
+
         <div class="row mb-0">
             <div class="col-md-6 offset-md-4">
                 <button type="submit" class="btn btn-primary">

dd7dc756d7350122cb5617b272223f21ebf2fdaf
MOMERO13 <18690293@tecvalles.mx>
2022-10-27 13:30:08 -0500

    Creacion del selected para cursos

--- resources/views/costos/create.blade.php
+++ resources/views/costos/create.blade.php
@@ -9,6 +9,26 @@
         @csrf
 
         <div class="row mb-3">
+            <label for="curso_id" class="col-md-4 col-form-label text-md-end">Nombre del curso</label>
+    
+            <div class="col-md-6">
+                <select class="form-select" id="curso_id" name="curso_id" required>
+                    <option selected>Elija un curso</option>
+                    @forelse ($cursos as $item)
+                        <option value="{{$item->id}}"> {{$item->nombre}}</option> 
+                       @empty
+                       <option>No hay cursos dados de alta</option>
+
+                    @endforelse
+                    
+                  </select>
+
+
+            </div>
+        </div>
+
+
+        <div class="row mb-3">
             <label for="periodo" class="col-md-4 col-form-label text-md-end">Periodo</label>
 
             <div class="col-md-6">

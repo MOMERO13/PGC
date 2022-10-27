3655082b8d0a34762fa8e962be009671d056364b
MOMERO13 <18690293@tecvalles.mx>
2022-10-27 13:46:52 -0500

    insersion del timestamps en BD

--- database/migrations/2022_10_04_225029_create_cursos_table.php
+++ database/migrations/2022_10_04_225029_create_cursos_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('nombre');
             $table->string('objetivo')->nullable();
             $table->string('descripcion')->nullable();
+            $table->timestamps();
         });
     }
 

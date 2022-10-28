b4b90f631787c9e170c37a3e7f760b50a3c2cd40
MOMERO13 <18690293@tecvalles.mx>
2022-10-27 19:20:52 -0500

    Nueva tabla de costos para visualisar los datos de formularios

---
+++ resources/views/costos/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('content')
+
+<x-section>  
+    <x-slot:columnas>10</x-slot>
+    <x-slot:titulo>Listado de Costos
+        <div class="float-end">
+            <a class="btn btn-outline-success" href="{{route('costo.create')}}" role="button">Nuevo</a>
+           </div>
+    </x-slot>
+    
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Periodo</th>
+                <th>Curso</th>			
+                <th>Costo derechohabiente</th>
+                <th>Costo publico</th>
+                <th>Opciones</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @forelse ($costos as $item)
+            <tr>			
+                <td>{{$item->periodo}}</td>
+                <td>{{$item->curso->nombre}}</td>
+                <td>${{$item->costo_derechohabiente}}</td>
+                <td>${{$item->costo_publico}}</td>
+                <td>--</td>
+            </tr>
+            @empty
+               <tr>
+                <td colspan="5">No hay costos registrados</td>
+               </tr> 
+            @endforelse
+
+        </tbody>
+    </table>
+
+
+</x-section>
+@endsection

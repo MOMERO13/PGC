2edad0e8198da54087bfedee476a2fbae6171a94
MOMERO13 <18690293@tecvalles.mx>
2022-10-29 15:33:51 -0500

    Creacion de llaves foraneas

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -11,12 +11,13 @@ class Grupo extends Model
     use HasFactory;
     protected $table = 'grupos';
     protected $fillable = [
-        
+        'curso_id',
+        'instructor_id',
         'fecha_inicio',
         'fecha_fin',
         'dias',
         'hora_inicio',
         'hora_fin',
-        'capacidad'				
+        'capacidad',				
     ];
 }

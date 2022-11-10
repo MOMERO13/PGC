183b5dc20c9f4aa3f5e813897aafec7afe791bb4
MOMERO13 <18690293@tecvalles.mx>
2022-11-10 15:05:35 -0600

    Utilizacion de la libreria Auth para el uso de la funcion de autentificacion

--- app/Http/Controllers/DependienteController.php
+++ app/Http/Controllers/DependienteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Dependiente;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DependienteController extends Controller
 {

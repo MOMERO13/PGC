9dce09971c88cf4875dc90861cdd6efdfc996cfa
MOMERO13 <18690293@tecvalles.mx>
2022-11-10 16:41:18 -0600

    Cambio de nombre al boton de redireccion

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -5,7 +5,7 @@
     <x-slot:columnas>10</x-slot>
     <x-slot:titulo>Listado de Cursos
         <div class="float-end">
-            <a class="btn btn-outline-success" href="{{route('curso.create')}}" role="button">Nuevo curso</a>
+            <a class="btn btn-outline-success" href="{{route('curso.create')}}" role="button">Nuevo Curso</a>
            </div>
 
     </x-slot>

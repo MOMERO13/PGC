9d863cff919502e2e2f0fb566295cb1faf696a0d
MOMERO13 <18690293@tecvalles.mx>
2022-11-17 14:19:38 -0600

    agregar campo grupo al formulario

--- resources/views/grupos/create.blade.php
+++ resources/views/grupos/create.blade.php
@@ -31,6 +31,19 @@
         </div>
 
 
+        <div class="row mb-3">
+            <label for="nombre_grupo" class="col-md-4 col-form-label text-md-end">Grupo</label>
+    
+            <div class="col-md-6">
+                <input id="nombre_grupo" type="text" class="form-control @error('nombre_grupo') is-invalid @enderror" name="nombre_grupo" value="{{ old('nombre_grupo') }}" required >
+    
+                @error('nombre_grupo')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
 
         <div class="row mb-3">
             <label for="fecha_inicio" class="col-md-4 col-form-label text-md-end">Fecha de inicio</label>

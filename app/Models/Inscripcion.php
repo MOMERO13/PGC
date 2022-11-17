ef00177ab29fba0e7b8c5b0c24682719c3735192
MOMERO13 <18690293@tecvalles.mx>
2022-11-17 14:41:44 -0600

    filable para inscripciones y funciones para llaves foraneas

--- app/Models/Inscripcion.php
+++ app/Models/Inscripcion.php
@@ -9,4 +9,26 @@ class Inscripcion extends Model
 {
     use HasFactory;
     protected $table = 'inscripciones';
+
+
+    protected $fillable = [
+        'grupo_id',
+        'curso_id',
+        'dependiente_id'
+    ];
+
+    public function grupo()
+    {
+        return $this->belongsTo(Grupo::class);
+    }
+
+    public function curso()
+    {
+        return $this->belongsTo(Curso::class);
+    }
+
+    public function inscrito()
+    {
+        return $this->belongsTo(Dependiente::class);
+    }
 }

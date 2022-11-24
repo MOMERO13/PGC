920bba8f38bfe22d34ae973b8c34e5420c8db8a9
MOMERO13 <18690293@tecvalles.mx>
2022-11-24 11:29:55 -0600

    Cambio de color a negro del navbar en caso de que el usuario sea administrador

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md shadow-sm {{(!is_null(Auth::user()) and Auth::user()->nivel==0)?'navbar-dark bg-dark':'navbar-light bg-white'}}">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}

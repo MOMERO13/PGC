a71b873d77957b43ad91f4ecb9ff6d7987d94835
MOMERO13 <18690293@tecvalles.mx>
2022-11-24 11:15:15 -0600

    uso de la clase nav con el fin de posicionar de una mejor forma los encabezados de home login y register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,15 +23,21 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="nav justify-content-end">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
+                    <li class="nav-item">
+                        <a href="{{ url('/home') }}" class="nav-link active">Home</a>
+                      </li>
+                        @else
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="nav-link active">Log in</a>
+                          </li>
+                       
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
+                        <li class="nav-item">
+                            <a href="{{ route('register') }}" class="nav-link active">Register</a>
+                        </li>
+                            @endif
                     @endauth
                 </div>
             @endif

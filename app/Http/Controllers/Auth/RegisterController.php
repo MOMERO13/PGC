4d58bf5ff51090e73ba4cfdbf59eb2e3acbc8c6e
MOMERO13 <18690293@tecvalles.mx>
2022-11-24 11:01:52 -0600

    Cambio del numero de nivel de 0 a 99 para usuarios

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'nivel' => 0,
+            'nivel' => 99,
         ]);
     }
 }
